a93b0b87d897c3793986540181de7c8f9b5b4b15
Branislav141 <branislavperovic3@gmail.com>
2023-04-11 23:37:01 +0200

    kreirana login forma

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import "./LoginPage.css";
 
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form className="Form">
+        <br />
+        <label>Username</label>
+        <input type="text" placeholder="enter your username"></input>
+
+        <label>Email</label>
+        <input
+          type="email"
+          className="email"
+          placeholder="enter your email"
+        ></input>
+
+        <input type="submit" className="loginbutton" value="Login" />
+        <hr></hr>
+
+        <div className="or">Or sign with:</div>
+        <br />
+        <div>
+          <button className="faceicon"></button>
+        </div>
+      </form>
+    </div>
+  );
 }
