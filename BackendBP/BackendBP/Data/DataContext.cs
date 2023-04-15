4a1bfcb62b8a9344d9156fb01c40d385605a854f
Branislav141 <branislavperovic3@gmail.com>
2023-04-15 22:54:40 +0200

    kreiranje backend app,dodavanje connection stringa ,kreiranje klase korsnika

---
+++ BackendBP/BackendBP/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using BackendBP.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendBP.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+           
+        }
+
+    }
+}
---
+++ BackendBP/BackendBP/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendBP.Models
+{
+    public class User
+    {
+        public String KorisnickoIme { get; set; }
+        public String Email { get; set; }
+        public String Lozinka { get; set; }
+        public String Ime { get; set; }
+        public String Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public String Adresa { get; set; }
+        public String Slika { get; set; }
+      
+
+    }
+}
